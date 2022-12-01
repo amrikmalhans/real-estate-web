e7fa4b3a767b0644d8631227660b29597f748292
amrik malhans <amrikdeveloper@gmail.com>
2022-12-01 11:54:33 +0530

    first commit

---
+++ pages/_document.tsx
@@ -0,0 +1,35 @@
+import React, { ReactElement } from "react";
+import Document, {
+  DocumentInitialProps,
+  DocumentContext,
+  Head,
+  NextScript,
+  Html,
+  Main,
+} from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): ReactElement {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>ABOUT PAGE</title>
+        <meta name="description" content="ABOUT PAGE" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>About page</h1>
+      {/* <Header /> */}
+    </div>
+  );
+}
